9f60911feca3869caf5f77c490180c9d33bba7da
Rameshwar Dhondge <rameshwardhondge@gmail.com>
2019-07-05 10:34:17 +0530

    File main.js created

---
+++ main.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var ur = require('url')
+http.createServer(function(req,res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("<h1>Hello there</h1>")
+    res.end();
+}).listen(8080);
