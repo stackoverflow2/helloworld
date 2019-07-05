3bfe37b570cc3c72d41251e7d431a4f9961156a9
Rameshwar Dhondge <rameshwardhondge@gmail.com>
2019-07-05 15:44:05 +0530

    express module added

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
+var http = require("http");
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 
-var http = require('http');
-var ur = require('url')
-http.createServer(function(req,res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("<h1>Hello there</h1>" );                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
-    res.end();
-}).listen(8080);
+
+
+// Running Server Details.
+var server = app.listen(8082, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at %s:%s Port", host, port)
+  });
+   
+   
+  app.get('/form', function (req, res) {
+    var html='';
+    html +="<body>";
+    html += "<form action='/thank'  method='post' name='form1'>";
+    html += "Name:</p><input type= 'text' name='name'>";
+    html += "Email:</p><input type='text' name='email'>";
+    html += "address:</p><input type='text' name='address'>";
+    html += "Mobile number:</p><input type='text' name='mobilno'>";
+    html += "<input type='submit' value='submit'>";
+    html += "<INPUT type='reset'  value='reset'>";
+    html += "</form>";
+    html += "</body>";
+    res.send(html);
+  });
+   
+  app.post('/thank', urlencodedParser, function (req, res){
+    var reply='';
+    reply += "Your name is" + req.body.name;
+    reply += "Your E-mail id is" + req.body.email; 
+    reply += "Your address is" + req.body.address;
+    reply += "Your mobile number is" + req.body.mobilno;
+    res.send(reply);
+   });
